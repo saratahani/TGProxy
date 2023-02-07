4a1dc5cb27f8db6bc04cb9b6134727733c1a08c6
saratahani <123387435+saratahani@users.noreply.github.com>
2023-02-07 18:50:19 +0330

    Update base.php

--- includes/base.php
+++ includes/base.php
@@ -2,12 +2,12 @@
 $root = 'http://'.$_SERVER['HTTP_HOST'].strtr(realpath(__DIR__),[$_SERVER['DOCUMENT_ROOT'] => null , 'includes' => null]);
 
 // source setting 
-define('channel',true); //is send proxy to channel?
+define('channel',false); //is send proxy to channel?
 define('grabber',true); //is grab proxy now?
 define('checking',true); // is delete bad proxies?
 // main varables
-define('TOKENBOT',''); # bot tokan from @botfather
-define('ADMINID' , ''); // bot admin unique id
+define('TOKENBOT','5985218499:AAFUn9Ad5MKGKQ3wdNiwGvuFLBjj5krZ_ZE'); # bot tokan from @botfather
+define('ADMINID' , '5615076487'); // bot admin unique id
 // channel info 
 $channel_id = ''; // unique id
 $username = ''; //username @example
