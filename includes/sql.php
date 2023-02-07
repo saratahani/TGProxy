0a68b743c6b57fec63051bb1af1076d282f33609
saratahani <123387435+saratahani@users.noreply.github.com>
2023-02-07 18:52:51 +0330

    Update sql.php

--- includes/sql.php
+++ includes/sql.php
@@ -1,3 +1,3 @@
 <?php 
-$conn = mysqli_connect('localhost','username','password','name');
+$conn = mysqli_connect('dpg-cfh6h3l3t397og7nbgr0-a','db_123_user','9rI6H3MOdt6uHXenhUhnQCGy5mbnnO7G','db_123');
 $conn->query("SET NAMES utf8mb4");
